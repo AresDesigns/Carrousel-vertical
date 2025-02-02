8b9d8542f32da74186d6000e76f3cc4652445039
Sergio Muñoz mate <80102148+AresDesigns@users.noreply.github.com>
2025-02-02 21:56:36 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://AresDesigns.github.io',
-  base: '/github-pages',
+  base: '/Carrousel-vertical',
   integrations: [
     tailwind(), ,
 ]
